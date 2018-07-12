1503e8bc7424ec488e4e07bda71deea8a81e5e36
Giuseppe Maxia <g.maxia@gmail.com>
2018-07-12 17:54:27 +0200

    bumped up version for error log explicit path fix

--- common/version.go
+++ common/version.go
@@ -15,7 +15,7 @@
 
 package common
 
-var VersionDef string = "1.8.0" // 2018-07-05
+var VersionDef string = "1.8.1" // 2018-07-12
 
 // Compatible version is the version used to mark compatible archives (templates, configuration).
 // It is usually major.minor.0, except when we are at version 0.x, when
