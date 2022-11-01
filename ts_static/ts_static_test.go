e1524b664d90e743a83ac12c5ba14506b4bab0df
Giuseppe Maxia <g.maxia@gmail.com>
2022-11-01 14:57:01 +0100

    Change mock-downloads test to run in 2 architectures

--- ts_static/ts_static_test.go
+++ ts_static/ts_static_test.go
@@ -16,10 +16,17 @@
 package ts_static
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"testing"
 
 	"github.com/datacharmer/dbdeployer/cmd"
+	"github.com/datacharmer/dbdeployer/common"
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
@@ -32,7 +39,38 @@ func TestStaticScripts(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
-	os.Exit(testscript.RunMain(m, map[string]func() int{
+	var srv *http.Server
+	fileServerDone := &sync.WaitGroup{}
+
+	identity := common.BaseName(os.Args[0])
+	inMain := strings.Contains(identity, "ts_static.test")
+	if inMain {
+		fileServerDone.Add(1)
+		srv = startFileServer(fileServerDone, "./downloads-arch", 9000)
+	}
+	exitCode := testscript.RunMain(m, map[string]func() int{
 		"dbdeployer": cmd.Execute,
-	}))
+	})
+	if inMain && srv != nil && srv.Addr != "" {
+		if err := srv.Shutdown(context.Background()); err != nil {
+			fmt.Printf("error stopping file server: %s\n", err)
+			os.Exit(1)
+		}
+		fileServerDone.Wait()
+	}
+	os.Exit(exitCode)
+}
+
+func startFileServer(wg *sync.WaitGroup, directory string, port int) *http.Server {
+	fs := http.FileServer(http.Dir(directory))
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: fs}
+
+	go func() {
+		defer wg.Done()
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+
+	return srv
 }
